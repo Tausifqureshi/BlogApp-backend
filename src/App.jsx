598eba440c78b1f5b3074e136c5c94f18b8da2bb
Tausifqureshi <tausifqureshi504@gmail.com>
2024-11-30 09:37:25 +0530

    Exprestions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 function App() {
-  return <div>
- <h1>  App </h1>
-  </div>;
-} 
+  const myName = "John";
+  console.log(myName);
+  return (
+    <div>
+      <h1> App </h1>
+
+    </div>
+  );
+}
 
 export default App;
