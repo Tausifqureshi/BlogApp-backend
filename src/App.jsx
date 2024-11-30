a02da32599488828b038be691b9013317f58cf45
Tausifqureshi <tausifqureshi504@gmail.com>
2024-11-30 09:49:46 +0530

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from "react";
 function App() {
   const myName = "John";
   console.log(myName);
+  console.log("hello");
   return (
     <div>
-      <h1> App </h1>
-
+      <h1> name is : {myName}</h1>
     </div>
   );
 }
