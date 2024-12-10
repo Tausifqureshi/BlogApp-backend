0fd3c5b26d4bbb39915b6a6e779a73a4e43ccb8f
Tausifqureshi <tausifqureshi504@gmail.com>
2024-12-10 17:23:17 +0530

    App-Blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import {createBrowserRouter, RouterProvider, Route,createRoutesFromElements} from "react-router-dom";
-
 function App() {
-
-
   return (
     <div>
-     <h1></h1>
+    <h1 className="text-3xl font-bold text-center bg-red-500"> App-Blog </h1>
     </div>  
   );
 }
