ae97f8f231542797405455d99f0cd0dff95cd3d0
Tausifqureshi <tausifqureshi504@gmail.com>
2024-12-11 09:36:16 +0530

    App-Blog

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div>
-   <h1 className="text-3xl text-orange-900">App</h1>
+   <h1 className="text-7xl text-orange-900">App</h1>
 
     </div>  
   );
