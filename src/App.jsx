bf0ce4bf2c3282cde6b04f92b52f2bbd89b3dd17
Tausifqureshi <tausifqureshi504@gmail.com>
2024-12-21 00:25:36 +0530

    App-Blog

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 // import {createBrowserRouter, RouterProvider, Route,createRoutesFromElements} from "react-router-dom";
 function App() {
+  console.log(import.meta.env.VITE_REACT_APP_APPWRITE_URL);
   return (
     <div>
    <h1 className="text-4xl md:text-6xl text-center text-orange-900">App</h1>
