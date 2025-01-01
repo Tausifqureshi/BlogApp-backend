3a48a95ff4b40410d8d43fea3de23530de2343d4
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-01 23:23:51 +0530

    Add

---
+++ src/appwrite/database.jsx
@@ -0,0 +1,5 @@
+import {Client, Account, ID, Databases, Storage, Query } from "appwrite";
+
+import { config } from "../config"; // Importing configuration for Appwrite server URL, project ID, etc.
+
+
