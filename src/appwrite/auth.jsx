1901dd0244a293aa50aefc847ca43896a7aa685e
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-01 20:53:45 +0530

    AppWrigte

--- src/appwrite/auth.jsx
+++ src/appwrite/auth.jsx
@@ -21,28 +21,49 @@ export class AuthService {
       const userAccount = await this.account.create(ID, email, password, name);
       if (userAccount) {
         // other function called
+        return this.login({ email, password });
       } else {
         return userAccount;
       }
     } catch (error) {
-      console.log("Appwrite Service Signup::",error);
+      console.log("Appwrite Service Signup::", error);
       throw error;
     }
   }
 
-//   Login function: User ko login karne ke liye
- async login({ email, password }) {
+  //   Login function: User ko login karne ke liye
+  async login({ email, password }) {
     console.log("Appwrite Service Login::");
-try {
-//  return await this.account.createEmailPasswordSession(email, password);
- return await this.account.createEmailSession(email, password); 
-
-} catch ( error) {
-    console.log("Appwrite Service Login::",error);
-    throw error;
-}
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+      //  return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      console.log("Appwrite Service Login::", error);
+      throw error;
+    }
   }
- }
+
+  // Logout function: User ko logout karne ke liye
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite Service Logout::", error);
+    }
+
+    return null;
+  }
+
+  // createCurrentUser function: Current user ko fetch karne ke liye
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite Service Current User::", error);
+      throw error;
+    }
+  }
+}
 
 const authService = new AuthService();
 export default authService;
