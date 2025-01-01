1fde1ae4cfc7dc3178603264399a3e0554ebf1e1
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-01 18:37:57 +0530

    AppWrigte

--- src/appwrite/auth.jsx
+++ src/appwrite/auth.jsx
@@ -1,7 +1,17 @@
 import config from "../config";
- import { Client, Account, ID } from "appwrite";
+import { Client, Account, ID } from "appwrite";
 
- export class AuthService {}
+export class AuthService {
+  client = new Client();
+  account;
 
- const authService = new AuthService();
- export default authService;
+  constructor() {
+    this.client
+      .setEndpoint() // Appwrite server ka URL
+      .setProject(); // Project ID set karte hain
+    this.account = new Account(this.client);
+  }
+}
+
+const authService = new AuthService();
+export default authService;
