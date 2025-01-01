39cff1da4f263f2ee3bd8b5849dff75cb625aae2
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-01 18:43:24 +0530

    AppWrigte

--- src/appwrite/auth.jsx
+++ src/appwrite/auth.jsx
@@ -6,11 +6,25 @@ export class AuthService {
   account;
 
   constructor() {
+    console.log("AuthService constructor called");
     this.client
-      .setEndpoint() // Appwrite server ka URL
-      .setProject(); // Project ID set karte hain
+      .setEndpoint(config.appwigtUrl) // Appwrite server ka URL
+      .setProject(config.appwriteProjectId); // Project ID set karte hain
     this.account = new Account(this.client);
+    console.log("AuthService initialized with client and account");
   }
+
+  async getCurrentUser() {
+    try {
+     // Current user ko fetch karega
+     return await this.account.get();
+    } catch (error) {
+     console.log("Appwrite Service Current User::",error);
+     throw error;
+    }
+ 
+   //  return null; 
+   }
 }
 
 const authService = new AuthService();
