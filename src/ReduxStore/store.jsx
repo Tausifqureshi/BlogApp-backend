be98ad69ca6a59b7f4fe82038677f4de189afcff
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-05 19:56:02 +0530

    Add

--- src/ReduxStore/store.jsx
+++ src/ReduxStore/store.jsx
@@ -1,10 +1,11 @@
-import  { configureStore } from "@reduxjs/toolkit";
-import {authSlice} from "./authSlice"
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./authSlice";    
+
 const store = configureStore({
     reducer: {
-        // Add your reducers here
         auth: authSlice,
-    }
-})
+    },
+   
+});
 
-export default store
+export default store;
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import store from './ReduxStore/store.jsx'
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+ <Provider store={store}>
   <App />
+ </Provider> 
+
+
+
+// {/* <App /> */}
+
+
 )
 
