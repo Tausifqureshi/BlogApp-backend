68c8aaad4104dc21a68a4aac5fa99bc46e26b5b6
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-06 10:41:05 +0530

    Appwrigte

--- src/components/container/Container.jsx
+++ src/components/container/Container.jsx
@@ -1,16 +1,7 @@
 import React from "react";
 
-function Container() {
-  return <div className="w-full max-w-7xl mx-auto px-4">Container</div>;
+function Container({ children }) {
+  return <div className="w-full max-w-7xl mx-auto px-4">{children}</div>;
 }
 
-export default Container; 
-
-
-
-
-
-
-
-
-
+export default Container;
