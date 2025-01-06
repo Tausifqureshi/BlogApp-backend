3216a448f92f25387c936c4e542ab08b9892e6a3
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-06 12:18:29 +0530

    A

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Logo from "./Logo/Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
 
 
-export { Header, Footer };
+export { Header, Footer, Logo, Container, LogoutBtn };
