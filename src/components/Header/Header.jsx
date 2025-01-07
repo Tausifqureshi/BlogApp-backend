f4de29fac3a6d01bbf1bfb453f4126cdbed55821
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-07 10:47:37 +0530

    Appwrighte

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 function Header() {
   const authStatus = useSelector((state) => state.auth.status);
   console.log("authStatus", authStatus);
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const navItems = [
     {
@@ -51,7 +51,7 @@ function Header() {
               item.active ? (
                 <li key={item.name}>
                   <button
-                    // onClick={() => navigate(item.slug)}
+                    onClick={() => navigate(item.slug)}
                     className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
                   >
                     {item.name}
