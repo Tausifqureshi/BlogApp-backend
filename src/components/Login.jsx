36ee57c42cb1cd7fb38f76c593864f0240ba9941
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-08 22:28:19 +0530

    Appwighte

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -40,9 +40,11 @@ function Login() {
             <Logo width="100%" />
           </span>
         </div>
+
         <h2 className="text-center text-2xl font-bold leading-tight">
           Sign in to your account
         </h2>
+
         <p className="mt-2 text-center text-base text-black/60">
           Don&apos;t have any account?&nbsp;
           <Link
@@ -51,7 +53,10 @@ function Login() {
           >
             Sign Up
           </Link>
+
         </p>
+
+        
         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
         <form onSubmit={handleSubmit(login)} className="mt-8">
           <div className="space-y-5">
