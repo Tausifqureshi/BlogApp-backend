d6c500490959d0cb2ce86e4a091f0849599846aa
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-12 23:16:28 +0530

    Appwighte

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -5,13 +5,19 @@ import { useSelector } from "react-redux";
 import databaseService from "../../appwrite/database";
 import { Input, Button, Select, RTE } from "../index";
 
-function PostForm() {
+function PostForm({post}) { //jo bbi is form ko use kar re ga waha se hi post ka data aa jaega props ke through. us post ko ham destucture kar ke nikal re post ko data ko use kar sakein.
+
   const navigate = useNavigate();
   const user = useSelector((state) => state.auth.user);
   console.log("user Post Form Page", user);
-  const {register,handleSubmit, control, watch, setValue, getValues} = useForm({
+
+  const {register, handleSubmit, control, watch, setValue, getValues} = useForm({
     defaultValues:{
-     title: "",
+      title: post?.title ||"",
+      slug: post?. $id ||"",
+      content:post?. content ||  "",
+      status:post?.status || "active",
+
     }
   });
 
