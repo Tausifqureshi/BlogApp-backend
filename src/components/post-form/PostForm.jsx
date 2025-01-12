7795c9b2f3b83212858c0ebbb8fb572336c9d7ce
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-13 00:43:34 +0530

    A

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -55,12 +55,95 @@ function PostForm({post}) { //jo bbi is form ko use kar re ga waha se hi post ka
       // post hai tu 
       if (newPost) {
         const fileId = file.$id;
-        data.featuredImage = fileId; //featuredImage ke ander fileid ko me save karna hoga
+        data.featuredImage = fileId; //featuredImage ke ander fileid ko me save karna hoga.
+        const createdPost = await databaseService.createPost({
+          ...data,
+          userId: userData.$id, //userId ke ander user ki data set karna hoga. userId createPost se milra hai.
+        })
+
+        // Post ko navigate karna hoga
+        if (createdPost) {
+          navigate(`/post/${createdPost.$id}`);
+          
+        }
       }
     }
 
   }
 
+  // Slug transform
+      const slugTransform = useCallback((value) => { //
+          if (value && typeof value === "string")
+              return value
+                  .trim()
+                  .toLowerCase()   
+                  .replace(/[^a-zA-Z\d\s]+/g, "-")
+                  .replace(/\s/g, "-");
+  
+          return "";  
+      }, []);
+
+      // useEffect
+        React.useEffect(() => {
+              const subscription = watch((value, { name }) => {
+                  if (name === "title") {
+                      setValue("slug", slugTransform(value.title), { shouldValidate: true });
+                  }
+              });
+      
+              return () => subscription.unsubscribe();
+          }, [watch, slugTransform, setValue]);
+
+
+             return (
+                  <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
+                      <div className="w-2/3 px-2">
+                          <Input
+                              label="Title :"
+                              placeholder="Title"
+                              className="mb-4"
+                              {...register("title", { required: true })}
+                          />
+                          <Input
+                              label="Slug :"
+                              placeholder="Slug"
+                              className="mb-4"
+                              {...register("slug", { required: true })}
+                              onInput={(e) => {
+                                  setValue("slug", slugTransform(e.currentTarget.value), { shouldValidate: true });
+                              }}
+                          />
+                          <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} />
+                      </div>
+                      <div className="w-1/3 px-2">
+                          <Input
+                              label="Featured Image :"
+                              type="file"
+                              className="mb-4"
+                              accept="image/png, image/jpg, image/jpeg, image/gif"
+                              {...register("image", { required: !post })}
+                          />
+                          {post && (
+                              <div className="w-full mb-4">
+                                  <img
+                                      src={appwriteService.getFilePreview(post.featuredImage)}
+                                      alt={post.title}
+                                      className="rounded-lg"
+                                  />
+                              </div>
+                          )}
+                          <Select
+                              options={["active", "inactive"]}
+                              label="Status"
+                              className="mb-4"
+                              {...register("status", { required: true })}
+                          />
+                          <Button type="submit" bgColor={post ? "bg-green-500" : undefined} className="w-full">
+                              {post ? "Update" : "Submit"}
+                          </Button>
+                      </div>
+                  </form>
+              );
  
   return (
   <div>
