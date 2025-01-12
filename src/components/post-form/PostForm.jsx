844a6faec3c210ae5bbb692f2e0a5935fcaaf2cc
Tausifqureshi <tausifqureshi504@gmail.com>
2025-01-12 11:04:54 +0530

    Aop

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -3,10 +3,10 @@ import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import databaseService from "../../appwrite/database";
+import { Input, Button, Select } from "../index";
 
 function PostForm() {
- 
-  return <div>PostForm</div>;
+  return <div></div>;
 }
 
 export default PostForm;
